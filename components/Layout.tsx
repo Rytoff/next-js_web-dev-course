6f6d0148e2cb3f77b7f568a8862435d045565780
Rytoff <roman.rytoff@gmail.com>
2022-02-04 18:29:31 +0200

    some bugs fixed

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactNode } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Head from "next/head";
 
 type layoutProps = {
   children: ReactNode
@@ -8,6 +9,10 @@ type layoutProps = {
 
 const Layout:FC<layoutProps> = ({ children }) => (
   <>
+  <Head>
+    <meta name="description" content="next.js course for beginers" />
+    <meta name="keywords" content="next.js, react, javascript, node.js" />
+  </Head>
     <Header />
     {children}
     <Footer />
