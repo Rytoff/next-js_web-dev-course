3bb967e426fc0e79beb3a5281681260050efbffc
Rytoff <roman.rytoff@gmail.com>
2022-02-04 16:16:03 +0200

    API Creation done L10

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
-import Head from "next/head";
-import Heading from "../components/Heading";
-import styles from "../styles/Home.module.scss";
+import Head from 'next/head'
+import Heading from '../components/Heading'
+import Socials from '../components/Socials'
+// import
+import styles from '../styles/Home.module.scss'
 
-const Home = () => (
+export const getStaticProps = async () => {
+  const res = await fetch(`${process.env.API_HOST}/socials/`)
+  const data = await res.json()
+
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+  return {
+    props: { socials: data },
+  }
+}
+
+const Home = ({ socials }) => (
   <div className={styles.wrapper}>
     <Head>
       <title>Home</title>
     </Head>
     <Heading text="Next.js Application" />
+    <Socials socials={socials} />
   </div>
-);
+)
 
-export default Home;
+export default Home
